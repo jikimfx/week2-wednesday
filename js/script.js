e8fc75635613bdcab90d5f053aba6cde1fb3a779
jikimfx <jikimfx@gmail.com>
2020-05-14 06:48:50 +0700

    Gameplay update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
-let computerNum = Math.floor(Math.random() * 100) + 1;
-let history = [];
+let computerNum = 0;
+let guesses = [];
+let previousGuessNum = 0;
 
-console.log("this is what computer chose", computerNum);
-function guess() {
+function generateRandomNumber() { computerNum = Math.floor(Math.random() * 50 + 1); console.log(computerNum); }
+function updatePreviousGuessNum() { previousGuessNum = guesses[guesses.length - 1]; }
+generateRandomNumber();
+
+submitButton.addEventListener("click", () => {
     let userNum = document.getElementById("guessNumber").value;
     let resultMessage = '';
 
     if (computerNum > userNum) {
-        resultMessage = "too low";
+        resultMessage = "Too Low!";
     }
     else if (computerNum < userNum) {
-        resultMessage = "too high";
+        resultMessage = "Too High!";
     }
     else if (computerNum === parseInt(userNum)) {
-        resultMessage = "correct";
+        resultMessage = "Correct!";
     }
 
-    history.push(userNum);
+    guesses.push(userNum);
+    updatePreviousGuessNum();
+    numberGuessesUpdate();
+
+    let repeat = false;
+    for (let i = 0; i < guesses.length - 1; i++) {
+        if (guesses[i] === guesses[guesses.length - 1]) {
+            repeat = true;
+        }
+    }
+
+    let num = parseInt(previousGuessNum);
 
     document.getElementById("resultArea").innerHTML = `${resultMessage}`;
-    document.getElementById("historyArea").innerHTML = `History: ${history}`;
-}
+    document.getElementById("previousGuesses").innerHTML = `Previous Guesses: ${guesses}`;
+})
+
+function numberGuessesUpdate () {
+    var guessNum = guesses.length;
+    document.getElementById("remainingGuesses").innerHTML = `${5 - guessNum}`;
+    if (guessNum === 5) {
+        alert("Game Over! The correct answer was " + computerNum);
+        resetGame();
+    }
+};
+
+function resetGame() {
+    guesses = [];
+    generateRandomNumber();
+    $('#previousGuesses').empty();
+    document.getElementById("resultArea").innerHTML = ``;
+    $('#remainingGuesses').text(5);
+};
